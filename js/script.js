16669f50cb2ac77b32e54b47ea69250d992cba1e
Anna Strzała <strzala.ania@gmail.com>
2019-01-24 14:38:25 +0100

    Fix script for mobile sidebar

--- js/script.js
+++ js/script.js
@@ -1,43 +1,7 @@
-//chart
-
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-
-    type: 'bar',
-    responsive: 'true',
-    data: {
-
-        labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
-
-        datasets: [{
-
-            label: "Signups",
-
-            backgroundColor: '#8DBEC8',
-            borderColor: '#8DBEC8',
-
-            data: [52, 51, 41, 94, 26, 6, 72, 9, 21, 88],
-        },
-        {
-            label: "FTD",
-            backgroundColor: '#F29E4E',
-            borderColor: '#F29E4E',
-            data: [6, 72, 1, 0, 47, 11, 50, 44, 63, 76],
-        },
-        {
-            label: "Earned",
-            backgroundColor: '#71B374',
-            borderColor: '#71B374',
-            data: [59, 49, 68, 90, 67, 41, 13, 38, 48, 48],
-
-            hidden: true,
-        }
-        ]
-    },
-});
-
+'use strict';
 function toggleMenu(visible) {
-    document.querySelector('sidebar').classList.toggle('show', visible)
+    document.querySelector('.sidebar').classList.toggle('show', visible),
+        document.querySelector('.sidebar-mobile').classList.toggle('show', visible)
 }
 
 document.querySelector('.toggle').addEventListener('click', function (e) {
