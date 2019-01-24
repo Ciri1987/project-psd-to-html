4c4067302b9a7bf2a8fe2814be446e81a071b05e
Anna Strzała <strzala.ania@gmail.com>
2019-01-24 10:54:58 +0100

    Some cosmetic fix

--- js/script.js
+++ js/script.js
@@ -4,33 +4,34 @@ var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
 
     type: 'bar',
+    responsive: 'true',
     data: {
 
         labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
 
         datasets: [{
 
-                label: "Signups",
+            label: "Signups",
 
-                backgroundColor: '#8DBEC8',
-                borderColor: '#8DBEC8',
+            backgroundColor: '#8DBEC8',
+            borderColor: '#8DBEC8',
 
-                data: [52, 51, 41, 94, 26, 6, 72, 9, 21, 88],
-            },
-            {
-                label: "FTD",
-                backgroundColor: '#F29E4E',
-                borderColor: '#F29E4E',
-                data: [6, 72, 1, 0, 47, 11, 50, 44, 63, 76],
-            },
-            {
-                label: "Earned",
-                backgroundColor: '#71B374',
-                borderColor: '#71B374',
-                data: [59, 49, 68, 90, 67, 41, 13, 38, 48, 48],
+            data: [52, 51, 41, 94, 26, 6, 72, 9, 21, 88],
+        },
+        {
+            label: "FTD",
+            backgroundColor: '#F29E4E',
+            borderColor: '#F29E4E',
+            data: [6, 72, 1, 0, 47, 11, 50, 44, 63, 76],
+        },
+        {
+            label: "Earned",
+            backgroundColor: '#71B374',
+            borderColor: '#71B374',
+            data: [59, 49, 68, 90, 67, 41, 13, 38, 48, 48],
 
-                hidden: true,
-            }
+            hidden: true,
+        }
         ]
     },
 });
